1270ed27a06084b7d1fc739e43059bca0a38213b
SecretOfNinja <85654389+SecretOfNinja@users.noreply.github.com>
2023-12-03 09:46:28 +0200

    Delete src directory

--- src/components/Header/TabButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function TabButton({id,children}) {
-  let target= '#' + id;
-  return (
-    <li><a href={target}><button>{children}</button></a></li>
-  )
-}
